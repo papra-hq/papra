a140d741d7c0de50a6b07d86a1dc31a0822a3775
Joshua Anderson <andersmmg@gmail.com>
2025-04-27 10:22:04 -0600

    fix(b2): remove unnecessary try-catches

--- apps/papra-server/src/modules/documents/storage/drivers/b2/b2.storage-driver.ts
+++ apps/papra-server/src/modules/documents/storage/drivers/b2/b2.storage-driver.ts
@@ -16,41 +16,32 @@ export const b2StorageDriverFactory = defineStorageDriver(async ({ config }) => 
   return {
     name: B2_STORAGE_DRIVER_NAME,
     saveFile: async ({ file, storageKey }) => {
-      try {
-        await b2Client.authorize();
-        const getUploadUrl = await b2Client.getUploadUrl({
-          bucketId,
-        });
-        const upload = await b2Client.uploadFile({
-          uploadUrl: getUploadUrl.data.uploadUrl,
-          uploadAuthToken: getUploadUrl.data.authorizationToken,
-          fileName: storageKey,
-          data: Buffer.from(await file.arrayBuffer()),
-        });
-        if (upload.status !== 200) {
-          throw new Error('Error uploading file');
-        }
-        return { storageKey };
-      } catch (_) {
+      await b2Client.authorize();
+      const getUploadUrl = await b2Client.getUploadUrl({
+        bucketId,
+      });
+      const upload = await b2Client.uploadFile({
+        uploadUrl: getUploadUrl.data.uploadUrl,
+        uploadAuthToken: getUploadUrl.data.authorizationToken,
+        fileName: storageKey,
+        data: Buffer.from(await file.arrayBuffer()),
+      });
+      if (upload.status !== 200) {
         throw new Error('Error uploading file');
       }
       return { storageKey };
     },
     getFileStream: async ({ storageKey }) => {
-      try {
-        await b2Client.authorize();
-        const response = await b2Client.downloadFileByName({
-          bucketName,
-          fileName: storageKey,
-          responseType: 'stream',
-        });
-        if (!response.data) {
-          throw new Error('File not found or has no content');
-        }
-        return { fileStream: response.data };
-      } catch (_) {
-        throw new Error('Error getting file');
+      await b2Client.authorize();
+      const response = await b2Client.downloadFileByName({
+        bucketName,
+        fileName: storageKey,
+        responseType: 'stream',
+      });
+      if (!response.data) {
+        throw new Error('File not found or has no content');
       }
+      return { fileStream: response.data };
     },
     deleteFile: async ({ storageKey }) => {
       await b2Client.hideFile({
