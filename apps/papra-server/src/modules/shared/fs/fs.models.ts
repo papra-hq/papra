ed4d7e4a00b2ca2c7fe808201c322f957d6ed990
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-09-10 22:48:56 +0200

    fix(folder-ingestion): allow cross docker volume file moving (#493)

---
+++ apps/papra-server/src/modules/shared/fs/fs.models.ts
@@ -0,0 +1,12 @@
+import { isNil } from '../utils';
+
+export function isCrossDeviceError({ error }: { error: Error & { code?: unknown } }) {
+  if (isNil(error.code) || typeof error.code !== 'string') {
+    return false;
+  }
+
+  return [
+    'EXDEV', // Linux based OS (see `man rename`)
+    'ERROR_NOT_SAME_DEVICE', // Windows
+  ].includes(error.code);
+}
