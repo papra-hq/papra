5ae14716fe861b65839c4d7b8be481dc75002a3f
jodli <jobst87@gmail.com>
2025-10-09 17:23:50 +0200

    feat: add run now button for tagging rules
    Allow users to apply existing tagging rules to all documents
    in their organization. This helps when rules are created after
    documents have already been imported.
    
    Fixes #251

--- apps/papra-server/src/modules/tasks/tasks.definitions.ts
+++ apps/papra-server/src/modules/tasks/tasks.definitions.ts
@@ -6,10 +6,12 @@ import { registerExtractDocumentFileContentTask } from '../documents/tasks/extra
 import { registerHardDeleteExpiredDocumentsTask } from '../documents/tasks/hard-delete-expired-documents.task';
 import { registerExpireInvitationsTask } from '../organizations/tasks/expire-invitations.task';
 import { registerPurgeExpiredOrganizationsTask } from '../organizations/tasks/purge-expired-organizations.task';
+import { registerApplyTaggingRuleToDocumentsTask } from '../tagging-rules/tasks/apply-tagging-rule-to-documents.task';
 
 export async function registerTaskDefinitions({ taskServices, db, config, documentsStorageService }: { taskServices: TaskServices; db: Database; config: Config; documentsStorageService: DocumentStorageService }) {
   await registerHardDeleteExpiredDocumentsTask({ taskServices, db, config, documentsStorageService });
   await registerExpireInvitationsTask({ taskServices, db, config });
   await registerPurgeExpiredOrganizationsTask({ taskServices, db, config, documentsStorageService });
   await registerExtractDocumentFileContentTask({ taskServices, db, documentsStorageService });
+  await registerApplyTaggingRuleToDocumentsTask({ taskServices, db });
 }
