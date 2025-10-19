cb22f5a553ee2c066b7c23d63f8854b9f79cf18c
Razvan M. <76774976+iRazvan2745@users.noreply.github.com>
2025-10-19 20:26:35 +0300

    Update documents-list.component.tsx

--- apps/papra-client/src/modules/documents/components/documents-list.component.tsx
+++ apps/papra-client/src/modules/documents/components/documents-list.component.tsx
@@ -5,16 +5,42 @@ import type { Document } from '../documents.types';
 import type { Tag } from '@/modules/tags/tags.types';
 import { formatBytes } from '@corentinth/chisels';
 import { A } from '@solidjs/router';
-import { createSolidTable, flexRender, getCoreRowModel, getPaginationRowModel } from '@tanstack/solid-table';
+import {
+  createSolidTable,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+} from '@tanstack/solid-table';
 import { For, Match, Show, Switch } from 'solid-js';
 import { timeAgo } from '@/modules/shared/date/time-ago';
 import { cn } from '@/modules/shared/style/cn';
 import { TagLink } from '@/modules/tags/components/tag.component';
 import { Button } from '@/modules/ui/components/button';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/modules/ui/components/select';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/modules/ui/components/table';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/modules/ui/components/tooltip';
-import { getDocumentIcon, getDocumentNameExtension, getDocumentNameWithoutExtension } from '../document.models';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/modules/ui/components/select';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/modules/ui/components/table';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/modules/ui/components/tooltip';
+import {
+  getDocumentIcon,
+  getDocumentNameExtension,
+  getDocumentNameWithoutExtension,
+} from '../document.models';
 import { DocumentManagementDropdown } from './document-management-dropdown.component';
 import { useI18n } from '@/modules/i18n/i18n.provider';
 
@@ -23,32 +49,45 @@ type Pagination = {
   pageSize: number;
 };
 
-
 export const createdAtColumn: ColumnDef<Document> = {
   header: () => {
     const { t } = useI18n();
-    return t('documents.list.table.headers.created');
+    return <span>{t('documents.list.table.headers.created')}</span>;
   },
   accessorKey: 'createdAt',
-  cell: data => <div class="text-muted-foreground hidden sm:block" title={data.getValue<Date>().toLocaleString()}>{timeAgo({ date: data.getValue<Date>() })}</div>,
+  cell: (data) => (
+    <div
+      class="text-muted-foreground hidden sm:block"
+      title={data.getValue<Date>().toLocaleString()}
+    >
+      {timeAgo({ date: data.getValue<Date>() })}
+    </div>
+  ),
 };
 
 export const deletedAtColumn: ColumnDef<Document> = {
   header: () => {
     const { t } = useI18n();
-    return t('documents.list.table.headers.deleted');
+    return <span>{t('documents.list.table.headers.deleted')}</span>;
   },
   accessorKey: 'deletedAt',
-  cell: data => <div class="text-muted-foreground hidden sm:block" title={data.getValue<Date>().toLocaleString()}>{timeAgo({ date: data.getValue<Date>() })}</div>,
+  cell: (data) => (
+    <div
+      class="text-muted-foreground hidden sm:block"
+      title={data.getValue<Date>().toLocaleString()}
+    >
+      {timeAgo({ date: data.getValue<Date>() })}
+    </div>
+  ),
 };
 
 export const standardActionsColumn: ColumnDef<Document> = {
   header: () => {
     const { t } = useI18n();
-    return t('documents.list.table.headers.actions');
+    return <span>{t('documents.list.table.headers.actions')}</span>;
   },
   id: 'actions',
-  cell: data => (
+  cell: (data) => (
     <div class="flex items-center justify-end">
       <DocumentManagementDropdown document={data.row.original} />
     </div>
@@ -58,15 +97,13 @@ export const standardActionsColumn: ColumnDef<Document> = {
 export const tagsColumn: ColumnDef<Document> = {
   header: () => {
     const { t } = useI18n();
-    return t('documents.list.table.headers.tags');
+    return <span>{t('documents.list.table.headers.tags')}</span>;
   },
   accessorKey: 'tags',
-  cell: data => (
+  cell: (data) => (
     <div class="text-muted-foreground hidden sm:flex flex-wrap gap-1">
       <For each={data.getValue<Tag[]>()}>
-        {tag => (
-          <TagLink {...tag} class="text-xs" />
-        )}
+        {(tag) => <TagLink {...tag} class="text-xs" />}
       </For>
     </div>
   ),
@@ -87,12 +124,19 @@ export const DocumentsPaginatedList: Component<{
     },
     columns: [
       {
-        header: () => t('documents.list.table.headers.file-name'),
+        header: () => (
+          <span>{t('documents.list.table.headers.file-name')}</span>
+        ),
         id: 'fileName',
-        cell: data => (
+        cell: (data) => (
           <div class="overflow-hidden flex gap-4 items-center">
             <div class="bg-muted flex items-center justify-center p-2 rounded-lg">
-              <div class={cn(getDocumentIcon({ document: data.row.original }), 'size-6 text-primary')}></div>
+              <div
+                class={cn(
+                  getDocumentIcon({ document: data.row.original }),
+                  'size-6 text-primary',
+                )}
+              ></div>
             </div>
 
             <div class="flex-1 flex flex-col gap-1 truncate">
@@ -100,20 +144,31 @@ export const DocumentsPaginatedList: Component<{
                 href={`/organizations/${data.row.original.organizationId}/documents/${data.row.original.id}`}
                 class="font-bold truncate block hover:underline"
               >
-                {getDocumentNameWithoutExtension({ name: data.row.original.name })}
+                {getDocumentNameWithoutExtension({
+                  name: data.row.original.name,
+                })}
               </A>
 
               <div class="text-xs text-muted-foreground lh-tight">
-                {[formatBytes({ bytes: data.row.original.originalSize, base: 1000 }), getDocumentNameExtension({ name: data.row.original.name })].filter(Boolean).join(' - ')}
-                {' '}
-                -
-                {' '}
+                {[
+                  formatBytes({
+                    bytes: data.row.original.originalSize,
+                    base: 1000,
+                  }),
+                  getDocumentNameExtension({
+                    name: data.row.original.name,
+                  }),
+                ]
+                  .filter(Boolean)
+                  .join(' - ')}{' '}
+                -{' '}
                 <Tooltip>
-                  <TooltipTrigger as={(tooltipProps: TooltipTriggerProps) => (
-                    <span {...tooltipProps}>
-                      {timeAgo({ date: data.row.original.createdAt })}
-                    </span>
-                  )}
+                  <TooltipTrigger
+                    as={(tooltipProps: TooltipTriggerProps) => (
+                      <span {...tooltipProps}>
+                        {timeAgo({ date: data.row.original.createdAt })}
+                      </span>
+                    )}
                   />
                   <TooltipContent>
                     {data.row.original.createdAt.toLocaleString()}
@@ -122,24 +177,8 @@ export const DocumentsPaginatedList: Component<{
               </div>
             </div>
           </div>
-
         ),
       },
-      // {
-      //   header: () => (<span class="hidden sm:block">Created at</span>),
-      //   accessorKey: 'createdAt',
-      //   cell: data => <div class="text-muted-foreground hidden sm:block" title={data.getValue<Date>().toLocaleString()}>{timeAgo({ date: data.getValue<Date>() })}</div>,
-      // },
-      // {
-      //   header: () => (<span class="block text-right">Actions</span>),
-      //   id: 'actions',
-      //   cell: data => (
-      //     <div class="flex items-center justify-end">
-      //       <DocumentManagementDropdown documentId={data.row.original.id} organizationId={data.row.original.organizationId} />
-      //     </div>
-      //   ),
-      // },
-
       ...(props.extraColumns ?? []),
     ],
     get rowCount() {
@@ -154,7 +193,6 @@ export const DocumentsPaginatedList: Component<{
       },
     },
     manualPagination: true,
-
   });
 
   return (
@@ -162,10 +200,9 @@ export const DocumentsPaginatedList: Component<{
       <Switch>
         <Match when={props.documentsCount > 0}>
           <Table>
-
             <TableHeader>
               <For each={table.getHeaderGroups()}>
-                {headerGroup => (
+                {(headerGroup) => (
                   <TableRow>
                     <For each={headerGroup.headers}>
                       {(header) => {
@@ -173,7 +210,10 @@ export const DocumentsPaginatedList: Component<{
                           <TableHead>
                             {header.isPlaceholder
                               ? null
-                              : flexRender(header.column.columnDef.header, header.getContext())}
+                              : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext(),
+                                )}
                           </TableHead>
                         );
                       }}
@@ -186,12 +226,15 @@ export const DocumentsPaginatedList: Component<{
             <TableBody>
               <Show when={table.getRowModel().rows?.length}>
                 <For each={table.getRowModel().rows}>
-                  {row => (
+                  {(row) => (
                     <TableRow data-state={row.getIsSelected() && 'selected'}>
                       <For each={row.getVisibleCells()}>
-                        {cell => (
+                        {(cell) => (
                           <TableCell>
-                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext(),
+                            )}
                           </TableCell>
                         )}
                       </For>
@@ -200,36 +243,34 @@ export const DocumentsPaginatedList: Component<{
                 </For>
               </Show>
             </TableBody>
-
           </Table>
 
           <Show when={props.showPagination ?? true}>
             <div class="flex flex-col-reverse items-center gap-4 sm:flex-row sm:justify-end mt-4">
               <div class="flex items-center space-x-2">
-                <p class="whitespace-nowrap text-sm font-medium">Rows per page</p>
+                <p class="whitespace-nowrap text-sm font-medium">
+                  Rows per page
+                </p>
                 <Select
                   value={table.getState().pagination.pageSize}
-                  onChange={value => value && table.setPageSize(value)}
+                  onChange={(value) => value && table.setPageSize(value)}
                   options={[15, 50, 100]}
-                  itemComponent={props => (
-                    <SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
+                  itemComponent={(props) => (
+                    <SelectItem item={props.item}>
+                      {props.item.rawValue}
+                    </SelectItem>
                   )}
                 >
                   <SelectTrigger class="h-8 w-[4.5rem]">
                     <SelectValue<string>>
-                      {state => state.selectedOption()}
+                      {(state) => state.selectedOption()}
                     </SelectValue>
                   </SelectTrigger>
                   <SelectContent />
                 </Select>
               </div>
               <div class="flex items-center justify-center whitespace-nowrap text-sm font-medium">
-                Page
-                {' '}
-                {table.getState().pagination.pageIndex + 1}
-                {' '}
-                of
-                {' '}
+                Page {table.getState().pagination.pageIndex + 1} of{' '}
                 {table.getPageCount()}
               </div>
               <div class="flex items-center space-x-2">
