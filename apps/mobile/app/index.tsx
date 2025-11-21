7d48bf1632bd4f60e119fbd0c256a2b30b21b126
jibraniqbal666 <jibran.iqbal@protonmail.com>
2025-11-21 22:03:42 +0100

    Keep the phone logged in

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { Redirect } from 'expo-router';
 import { configLocalStorage } from '@/modules/config/config.local-storage';
+import { useAuthClient } from '@/modules/api/providers/api.provider';
 
 export default function Index() {
   const query = useQuery({
     queryKey: ['api-server-url'],
     queryFn: configLocalStorage.getApiServerBaseUrl,
   });
+
+  const authClient = useAuthClient();
 
   const getRedirection = () => {
     if (query.isLoading) {
@@ -15,6 +18,10 @@ export default function Index() {
 
     if (query.isError || query.data == null) {
       return <Redirect href="/config/server-selection" />;
+    }
+
+    if (authClient.getCookie() != null) {
+      return <Redirect href="/(app)/(with-organizations)/(tabs)/list" />;
     }
 
     return <Redirect href="/auth/login" />;
