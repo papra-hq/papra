334fcbdee479384a07dfb10e6654aaec23c14e17
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-11-29 00:04:30 +0100

    refactor(search): mutualize search function in dedicated fts5 repository (#658)

--- apps/papra-server/src/modules/documents/document-search/database-fts5/database-fts5.document-search-provider.ts
+++ apps/papra-server/src/modules/documents/document-search/database-fts5/database-fts5.document-search-provider.ts
@@ -1,15 +1,15 @@
 import type { Database } from '../../../app/database/database.types';
 import type { DocumentSearchServices } from '../document-search.types';
-import { createDocumentsRepository } from '../../documents.repository';
 import { DATABASE_FTS5_DOCUMENT_SEARCH_PROVIDER_NAME } from './database-fts5.document-search-provider.constants';
+import { createDocumentSearchRepository } from './database-fts5.repository';
 
 export function createDatabaseFts5DocumentSearchServices({ db }: { db: Database }): DocumentSearchServices {
-  const documentsRepository = createDocumentsRepository({ db });
+  const documentsSearchRepository = createDocumentSearchRepository({ db });
 
   return {
     name: DATABASE_FTS5_DOCUMENT_SEARCH_PROVIDER_NAME,
     searchDocuments: async ({ searchQuery, organizationId, pageIndex, pageSize }) => {
-      const { documents } = await documentsRepository.searchOrganizationDocuments({ organizationId, searchQuery, pageIndex, pageSize });
+      const { documents } = await documentsSearchRepository.searchOrganizationDocuments({ organizationId, searchQuery, pageIndex, pageSize });
 
       return {
         searchResults: {
