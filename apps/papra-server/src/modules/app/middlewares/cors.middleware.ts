3efa68d61794ba73a73e26899e2cb8c47a94beae
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-01-26 15:49:53 +0100

    refactor(server): cleaned some useless lodash

--- apps/papra-server/src/modules/app/middlewares/cors.middleware.ts
+++ apps/papra-server/src/modules/app/middlewares/cors.middleware.ts
@@ -1,13 +1,12 @@
 import type { Config } from '../../config/config.types';
 import { cors } from 'hono/cors';
-import { first } from 'lodash-es';
 
 export function createCorsMiddleware({ config }: { config: Config }) {
   return cors({
     origin: (origin) => {
       const allowedOrigins = config.server.corsOrigins;
 
-      if (first(allowedOrigins) === '*' && allowedOrigins.length === 1) {
+      if (allowedOrigins[0] === '*' && allowedOrigins.length === 1) {
         return origin;
       }
 
