f7b03368d6edef7d82fd4870c068d009a1f53127
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-01-29 14:05:43 +0100

    fix(mobile): stop redirecting to org creation when invalid server (#833)

--- apps/mobile/src/modules/api/http.client.ts
+++ apps/mobile/src/modules/api/http.client.ts
@@ -1,5 +1,5 @@
-import type { FetchOptions, ResponseType } from 'ofetch';
-import { ofetch } from 'ofetch';
+import type { FetchOptions } from 'ofetch';
+import { FetchError, ofetch, ResponseType } from 'ofetch';
 
 export { ResponseType };
 export type HttpClientOptions<R extends ResponseType = 'json'> = Omit<FetchOptions<R>, 'baseURL'> & { url: string; baseUrl?: string };
@@ -9,4 +9,8 @@ export async function httpClient<A, R extends ResponseType = 'json'>({ url, base
     baseURL: baseUrl,
     ...rest,
   });
+}
+
+export function isHttpClientError(error: unknown): error is FetchError {
+  return error instanceof FetchError;
 }
