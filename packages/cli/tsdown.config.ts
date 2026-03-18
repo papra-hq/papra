91e33cbb1cc1c37ec972db2b06b261f2ad4d4891
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-03-18 12:44:56 +0100

    feat(cli): build executable

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'tsdown';
+import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
   entry: ['./src/cli.ts'],
-  dts: { sourcemap: true },
-  sourcemap: true,
-  clean: true,
-  exports: true, // auto update the package.json exports, main, module and types fields
+  // dts: { sourcemap: true },
+  // sourcemap: true,
+  // clean: true,
+  // exports: true, // auto update the package.json exports, main, module and types fields
+  exe: true,
+  deps: {
+    alwaysBundle: Object.keys(pkg.dependencies ?? {}),
+  },
 });
