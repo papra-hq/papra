4a10ace601d418ae619e179c95a766c91984b0f1
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-04-04 03:22:04 +0200

    refactor(server): custom in house omitUndefined (#1013)

--- apps/papra-server/src/modules/shared/objects.ts
+++ apps/papra-server/src/modules/shared/objects.ts
@@ -1,8 +1,24 @@
+type OmitUndefined<T> = {
+  [K in keyof T]: Exclude<T[K], undefined>;
+};
+
+export function omitUndefined<T extends Record<string, any>>(obj: T): OmitUndefined<T> {
+  const result = {} as OmitUndefined<T>;
+
+  for (const key in obj) {
+    if (obj[key] !== undefined && Object.hasOwn(obj, key) && Object.propertyIsEnumerable.call(obj, key)) {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
+}
+
 export function pick<T extends object, K extends keyof T>(obj: T, keys: K[] | readonly K[]): Pick<T, K> {
   const result = {} as Pick<T, K>;
 
   for (const key of keys) {
-    if (Object.hasOwn(obj, key)) {
+    if (Object.hasOwn(obj, key) && Object.propertyIsEnumerable.call(obj, key)) {
       result[key] = obj[key];
     }
   }
