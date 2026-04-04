39b8303472a56d036b6129d873ca5de0debe95c8
John Cuba <ekubetsky@gmail.com>
2026-04-04 13:13:24 +0300

    fix(client): remove close button

--- apps/papra-client/src/modules/ui/components/sheet.tsx
+++ apps/papra-client/src/modules/ui/components/sheet.tsx
@@ -59,23 +59,6 @@ export function SheetContent<T extends ValidComponent = 'div'>(props: Polymorphi
         {...rest}
       >
         {local.children}
-        <DialogPrimitive.CloseButton class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:(outline-none ring-1.5 ring-ring ring-offset-2) disabled:pointer-events-none bg-inherit transition-property-[opacity,box-shadow]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="h-4 w-4"
-          >
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M18 6L6 18M6 6l12 12"
-            />
-            <title>Close</title>
-          </svg>
-        </DialogPrimitive.CloseButton>
       </DialogPrimitive.Content>
     </DialogPrimitive.Portal>
   );
