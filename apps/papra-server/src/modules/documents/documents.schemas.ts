f8e62cc5385a2d09de942a3796680ee49b3f89e7
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-04-16 17:29:34 +0200

    refactor(server): cleanup no longer used zod schemas

--- apps/papra-server/src/modules/documents/documents.schemas.ts
+++ apps/papra-server/src/modules/documents/documents.schemas.ts
@@ -1,6 +1,6 @@
 import * as v from 'valibot';
 import { createRegexSchema } from '../shared/schemas/string.schemas';
-import { DOCUMENT_ID_REGEX } from './documents.constants';
+import { DOCUMENT_ID_REGEX, OCR_LANGUAGES } from './documents.constants';
 
 export const documentIdSchema = createRegexSchema(DOCUMENT_ID_REGEX);
 
@@ -14,4 +14,11 @@ export const updateDocumentBodySchema = v.pipe(
     data => data.name !== undefined || data.content !== undefined || data.documentDate !== undefined,
     'At least one of \'name\', \'content\', or \'documentDate\' must be provided',
   ),
+);
+
+export const ocrLanguagesSchema = v.array(v.picklist(OCR_LANGUAGES));
+export const stringCoercedOcrLanguagesSchema = v.pipe(
+  v.string(),
+  v.transform(value => value.split(',').map(lang => lang.trim())),
+  ocrLanguagesSchema,
 );
