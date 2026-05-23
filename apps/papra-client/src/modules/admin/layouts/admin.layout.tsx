f81d40628abcce7549bfc270b572e5d1772b85fe
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-05-23 22:55:47 +0200

    fix(client): prevent admin layout sidenav from being pushed out of view

--- apps/papra-client/src/modules/admin/layouts/admin.layout.tsx
+++ apps/papra-client/src/modules/admin/layouts/admin.layout.tsx
@@ -76,7 +76,7 @@ const AdminLayout: ParentComponent = (props) => {
           </Button>
         </header>
 
-        <div class="flex-1 min-h-0 flex flex-col md:rounded-tl-lg md:border-l border-t bg-background">
+        <div class="flex-1 min-h-0 overflow-auto md:rounded-tl-lg md:border-l border-t bg-background">
           {props.children}
         </div>
       </div>
