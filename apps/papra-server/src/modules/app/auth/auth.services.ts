d178643ea399a5043561fa89c44d720634e29ac5
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-08-07 16:38:16 +0200

    fix(auth): revoke on password reset (#1406)

--- apps/papra-server/src/modules/app/auth/auth.services.ts
+++ apps/papra-server/src/modules/app/auth/auth.services.ts
@@ -45,6 +45,7 @@ export function getAuth({
     },
     emailAndPassword: {
       enabled: config.auth.providers.email.isEnabled,
+      revokeSessionsOnPasswordReset: true,
       requireEmailVerification: config.auth.isEmailVerificationRequired,
       sendResetPassword: config.auth.isPasswordResetEnabled
         ? authEmailsServices.sendPasswordResetEmail
