ac61fa9b59c34d5653058e1655726da4e51f6cf9
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-09-01 17:59:24 +0200

    fix(auth): enforce registration disabled for email

--- apps/papra-server/src/modules/app/auth/auth.services.ts
+++ apps/papra-server/src/modules/app/auth/auth.services.ts
@@ -45,6 +45,7 @@ export function getAuth({
     },
     emailAndPassword: {
       enabled: config.auth.providers.email.isEnabled,
+      disableSignUp: !config.auth.isRegistrationEnabled,
       revokeSessionsOnPasswordReset: true,
       requireEmailVerification: config.auth.isEmailVerificationRequired,
       sendResetPassword: config.auth.isPasswordResetEnabled
